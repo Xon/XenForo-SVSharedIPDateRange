e57aabe7fa91a79562d6ab318ab72a15c52004ba
Xon <tazipper1@hotmail.com>
2015-07-27 08:06:14 +0800

    Minor cleanup

--- upload/library/SV/SharedIPDateRange/Listener.php
+++ upload/library/SV/SharedIPDateRange/Listener.php
@@ -4,11 +4,6 @@ class SV_SharedIPDateRange_Listener
 {
     public static function load_class($class, &$extend)
     {
-        switch ($class)
-        {
-            case 'XenForo_ControllerPublic_Member':
-                $extend[] = 'SV_SharedIPDateRange_XenForo_ControllerPublic_Member';
-                break;
-        }
+        $extend[] = 'SV_SharedIPDateRange_' . $class;
     }
 }
--- upload/library/SV/SharedIPDateRange/XenForo/ControllerPublic/Member.php
+++ upload/library/SV/SharedIPDateRange/XenForo/ControllerPublic/Member.php
@@ -2,31 +2,31 @@
 
 class SV_SharedIPDateRange_XenForo_ControllerPublic_Member extends XFCP_SV_SharedIPDateRange_XenForo_ControllerPublic_Member
 {
-	public function actionSharedIps()
-	{
-		$userId = $this->_input->filterSingle('user_id', XenForo_Input::UINT);
-		$user = $this->getHelper('UserProfile')->getUserOrError($userId);
+    public function actionSharedIps()
+    {
+        $userId = $this->_input->filterSingle('user_id', XenForo_Input::UINT);
+        $user = $this->getHelper('UserProfile')->getUserOrError($userId);
 
-		if (!$this->_getUserModel()->canViewIps())
-		{
-			return $this->responseNoPermission();
-		}
+        if (!$this->_getUserModel()->canViewIps())
+        {
+            return $this->responseNoPermission();
+        }
 
-		/** @var XenForo_Model_Ip $ipModel */
-		$ipModel = $this->getModelFromCache('XenForo_Model_Ip');
-		$users = $ipModel->getSharedIpUsers($user['user_id'], XenForo_Application::get('options')->spamCheckIpsDaysLimit);
+        /** @var XenForo_Model_Ip $ipModel */
+        $ipModel = $this->getModelFromCache('XenForo_Model_Ip');
+        $users = $ipModel->getSharedIpUsers($user['user_id'], XenForo_Application::get('options')->spamCheckIpsDaysLimit);
 
-		$viewParams = array(
-			'profileUser' => $user,
-			'users' => $users,
-		);
+        $viewParams = array(
+            'profileUser' => $user,
+            'users' => $users,
+        );
 
-		return $this->responseView(
-			'XenForo_ViewPublic_Member_SharedIps',
-			'member_shared_ips',
-			$viewParams
-		);
-	} 
+        return $this->responseView(
+            'XenForo_ViewPublic_Member_SharedIps',
+            'member_shared_ips',
+            $viewParams
+        );
+    }
 }
 
 
